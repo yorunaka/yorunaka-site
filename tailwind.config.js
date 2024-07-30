750c485814b97276def19c74aa4ffa848eccd0e9
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-07-30 19:57:18 +0700

    add:web icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['open-sans']
+    },
     container : {
       center: true,
       padding: '16px',
