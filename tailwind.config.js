15cc3f6705437a51cc95d7f6469599e8a2b30cec
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-09-05 20:42:16 +0700

    update: blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html','./blog.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['open-sans']
