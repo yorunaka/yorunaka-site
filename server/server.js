669068965f5aad53089dfb8d548f9c92e40dd4de
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-09-06 13:23:32 +0700

    update: blog

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +8,14 @@ app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
+    console.log(req.socket.server);
+    next();
 });
 
 app.listen(port);
+app.server = http.createServer(app)
+http.createServer(app).listen(3000), 'http://192.168.77.250';
+console.log('Server started at http://192.168.77.250:' + 3000);
 console.log('Server started at http://localhost:' + port);
+console.log('Press Ctrl+C to quit.');
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html','./blog.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['open-sans']
